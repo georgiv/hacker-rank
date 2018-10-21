e79533910a741312db1a076222ff3f82384bcd83
georgiv <gd.vassilev@gmail.com>
2018-10-22 01:24:01 +0300

    Remove test file

--- interview-preparation-kit/src/Demos.java
+++
@@ -1,8 +0,0 @@
-import java.util.HashMap;
-
-public class Demos {
-  public static void main(String[] args) {
-    HashMap<Integer, Integer> m = new HashMap<>();
-    System.out.println(m.get(6));
-  }
-}
