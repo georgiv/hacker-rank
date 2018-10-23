159c5300f381761a2f667c21bb4451861ecb64f7
georgiv <gd.vassilev@gmail.com>
2018-10-24 01:46:34 +0300

    Working on arrays...

--- interview-preparation-kit/src/arrays/NewYearChaos.java
+++ interview-preparation-kit/src/arrays/NewYearChaos.java
@@ -6,7 +6,22 @@ import java.io.InputStreamReader;
 
 public class NewYearChaos {
   static void minimumBribes(int[] q) {
-    
+    int allowed = 0;
+    int additional = 0;
+    for (int i = 0; i < q.length; i++) {
+      int move = q[i] - (i + 1);
+      if (move > 2) {
+        System.out.println("Too chaotic");
+        return;
+      }
+      move = Math.abs(move);
+      if (move > 2) {
+        additional += move - 2;
+        move = 2;
+      }
+      allowed += move;
+    }
+    System.out.println(allowed / 2 + additional);
   }
 
   public static void main(String[] args) throws IOException {
