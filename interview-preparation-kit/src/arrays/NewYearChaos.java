af5a9d295c29abaaebfb474f89a723cb0541806d
georgiv <gd.vassilev@gmail.com>
2018-10-23 23:14:37 +0300

    Working on arrays...

--- interview-preparation-kit/src/arrays/NewYearChaos.java
+++ interview-preparation-kit/src/arrays/NewYearChaos.java
@@ -1,11 +1,26 @@
 package arrays;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class NewYearChaos {
   static void minimumBribes(int[] q) {
-
+    
   }
 
-  public static void main(String[] args) {
-    
+  public static void main(String[] args) throws IOException {
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    int t = Integer.parseInt(in.readLine());
+
+    for (int i = 0; i < t; i++) {
+      int n = Integer.parseInt(in.readLine());
+      int[] ns = new int[n];
+      String[] nsLine = in.readLine().split(" ");
+      for (int j = 0; j < n; j++) {
+        ns[j] = Integer.parseInt(nsLine[j]);
+      }
+      minimumBribes(ns);
+    }
   }
 }
