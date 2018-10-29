78cbd5455ceb9b2937fc54f031bebbfe08af7e32
georgiv <gd.vassilev@gmail.com>
2018-10-29 12:23:32 +0200

    Swap Nodes completed

--- interview-preparation-kit/src/search/SwapNodes.java
+++ interview-preparation-kit/src/search/SwapNodes.java
@@ -3,10 +3,109 @@ package search;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
+
+class TreeNode {
+  private int value;
+  private int depth;
+  private TreeNode leftChild;
+  private TreeNode rightChild;
+
+  TreeNode(int value, int depth) {
+    this.value = value;
+    this.depth = depth;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  public int getDepth() {
+    return depth;
+  }
+
+  public TreeNode getLeftChild() {
+    return leftChild;
+  }
+
+  public void setLeftChild(TreeNode leftChild) {
+    this.leftChild = leftChild;
+  }
+
+  public TreeNode getRightChild() {
+    return rightChild;
+  }
+
+  public void setRightChild(TreeNode rightChild) {
+    this.rightChild = rightChild;
+  }
+}
 
 public class SwapNodes {
   static int[][] swapNodes(int[][] indexes, int[] queries) {
-    return null;
+    TreeNode root = new TreeNode(1, 1);
+
+    Queue<TreeNode> traverseQueue = new LinkedList<>();
+    traverseQueue.offer(root);
+    for (int i = 0; i < indexes.length; i++) {
+      TreeNode currentNode = traverseQueue.poll();
+      int currentDepth = currentNode.getDepth();
+
+      if (indexes[i][0] != -1) {
+        TreeNode leftChild = new TreeNode(indexes[i][0], currentDepth + 1);
+        currentNode.setLeftChild(leftChild);
+        traverseQueue.offer(leftChild);
+      }
+
+      if (indexes[i][1] != -1) {
+        TreeNode rightChild = new TreeNode(indexes[i][1], currentDepth + 1);
+        currentNode.setRightChild(rightChild);
+        traverseQueue.offer(rightChild);
+      }
+    }
+
+    int[][] res = new int[queries.length][];
+
+    for (int i = 0; i < queries.length; i++) {
+      Deque<TreeNode> traversePath = new ArrayDeque<>();
+      traversePath.offer(root);
+
+      while (!traversePath.isEmpty()){
+        TreeNode currentNode = traversePath.poll();
+        int currentDepth = currentNode.getDepth();
+
+        TreeNode leftChild = currentNode.getLeftChild();
+        if (leftChild != null) {
+          traversePath.offer(leftChild);
+        }
+        TreeNode rightChild = currentNode.getRightChild();
+        if (rightChild != null) {
+          traversePath.offer(rightChild);
+        }
+
+        if (currentDepth % queries[i] == 0) {
+          currentNode.setLeftChild(rightChild);
+          currentNode.setRightChild(leftChild);
+        }
+      }
+
+      List<Integer> path = new ArrayList<>();
+      inOrderTraversal(root, path);
+      res[i] = new int[path.size()];
+      for (int j = 0; j < path.size(); j++) {
+        res[i][j] = path.get(j);
+      }
+    }
+
+    return res;
+  }
+
+  static void inOrderTraversal(TreeNode root, List<Integer> path) {
+    if (root != null) {
+      inOrderTraversal(root.getLeftChild(), path);
+      path.add(root.getValue());
+      inOrderTraversal(root.getRightChild(), path);
+    }
   }
 
   public static void main(String[] args) throws IOException {
