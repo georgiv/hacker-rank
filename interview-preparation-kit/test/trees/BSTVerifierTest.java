919c4a53c39eb56f39124087ac3afcc019a75a60
georgiv <gd.vassilev@gmail.com>
2018-11-01 00:04:33 +0200

    One more tree completed

--- interview-preparation-kit/test/trees/BSTVerifierTest.java
+++ interview-preparation-kit/test/trees/BSTVerifierTest.java
@@ -13,7 +13,7 @@ public class BSTVerifierTest {
     root.right.left = new Node(5);
     root.right.right = new Node(6);
 
-    Assert.assertTrue(new BSTVerifier().checkBST(root));
+    Assert.assertFalse(new BSTVerifier().checkBST(root));
   }
 
   @Test
@@ -25,6 +25,39 @@ public class BSTVerifierTest {
     root.right.left = new Node(6);
     root.right.right = new Node(1);
 
-    Assert.assertTrue(new BSTVerifier().checkBST(root));
+    Assert.assertFalse(new BSTVerifier().checkBST(root));
+  }
+
+  @Test
+  public void checkBSTTest3() {
+    Node root = new Node(30);
+    root.left = new Node(5);
+    root.right = new Node(40);
+    root.right.left = new Node(10);
+    root.right.right = new Node(50);
+
+    Assert.assertFalse(new BSTVerifier().checkBST(root));
+  }
+
+  @Test
+  public void checkBSTTest4() {
+    Node root = new Node(8);
+    root.left = new Node(4);
+    root.left.left = new Node(2);
+    root.left.left.left = new Node(1);
+    root.left.left.right = new Node(3);
+    root.left.right = new Node(6);
+    root.left.right.left = new Node(5);
+    root.left.right.right = new Node(7);
+    root.right = new Node(13);
+    root.right.left = new Node(10);
+    root.right.left.left = new Node(9);
+    root.right.left.right = new Node(11);
+    root.right.right = new Node(14);
+    root.right.right = new Node(14);
+    root.right.right.left = new Node(12);
+    root.right.right.right = new Node(15);
+
+    Assert.assertFalse(new BSTVerifier().checkBST(root));
   }
 }
