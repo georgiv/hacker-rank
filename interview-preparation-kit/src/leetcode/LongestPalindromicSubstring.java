2c4e9f6507a7e055b1ba5c0ebc752c948d122a26
georgiv <gd.vassilev@gmail.com>
2018-11-12 14:21:24 +0200

    Longest palindrome solved

--- interview-preparation-kit/src/leetcode/LongestPalindromicSubstring.java
+++ interview-preparation-kit/src/leetcode/LongestPalindromicSubstring.java
@@ -1,49 +1,33 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class LongestPalindromicSubstring {
   public String longestPalindrome(String s) {
-    if (s == null || s.length() == 0 || s.length() == 1) return s;
+    if (s == null) return "";
 
-    Map<Character, List<Integer>> indices = new HashMap<>();
+    if (s.length() == 0 || s.length() == 1) return s;
 
     String res = s.substring(0, 1);
 
     for (int i = 0; i < s.length(); i++) {
-      char curr = s.charAt(i);
-
-      if (!indices.keySet().contains(curr)) indices.put(curr, new ArrayList<>());
-
-      List<Integer> currIndices = indices.get(curr);
-
-      if (currIndices.size() != 0) {
-        for (int j = 0; j < currIndices.size(); j++) {
-          int left = currIndices.get(j);
-          int right = i;
-          boolean noPalindrome = false;
-          while (left != right + 1 && left != right) {
-            if (right - left + 1 <= res.length()) {
-              noPalindrome = true;
-              break;
-            }
-            if (s.charAt(left) == s.charAt(right)) {
-              left++;
-              right--;
-            } else {
-              noPalindrome = true;
-              break;
-            }
-          }
-          if (noPalindrome) continue;
-          if (i - currIndices.get(j) + 1 > res.length()) res = s.substring(currIndices.get(j), i + 1);
+      int offset = 0;
+      while (i - offset >= 0 && i + offset < s.length()) {
+        if (s.charAt(i - offset) != s.charAt(i + offset)) {
+          break;
         }
+        offset++;
       }
+      offset--;
+      if (offset * 2 + 1 > res.length()) res = s.substring(i - offset, i + offset + 1);
 
-      indices.get(curr).add(i);
+      offset = 0;
+      while (i - offset >= 0 && i + offset + 1 < s.length()) {
+        if (s.charAt(i - offset) != s.charAt(i + offset + 1)) {
+          break;
+        }
+        offset++;
+      }
+      offset--;
+      if (offset * 2 + 2 > res.length()) res = s.substring(i - offset, i + offset + 2);
     }
 
     return res;
